d292b503af66ab48efa6aeb76e9d9bb7ad4c3873
Rahat <rahatalisoomro10@gmail.com>
2025-02-04 00:15:30 +0500

    ReadlineSync Example

---
+++ index.js
@@ -0,0 +1,10 @@
+import openai from './config/open-ai.js';
+import readlineSync from 'readline-sync';
+import colors from 'colors';
+
+async function main() {
+    console.log(colors.bold.green('Welcome to the Chatbot Program'));
+    console.log(colors.bold.green('You can start chatting with the bot.'));
+}
+
+main();
